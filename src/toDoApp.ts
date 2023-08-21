0cb9694903eda26489b06e3a83ced06f5f96bedd
eliezer bauer <lazerbauer@gmail.com>
2023-08-21 23:20:28 +0100

    first upload

---
+++ src/toDoApp.ts
@@ -0,0 +1,119 @@
+import { Todo } from "./interface/todo.interface";
+import { createTodoList } from "./toDo.js";
+const {
+  getToDos,
+  addToDos,
+  removeToDos,
+  changeToDos,
+  removeAll,
+  getToDoById,
+  saveTodo,
+} = createTodoList();
+// render
+const todoInput = document.getElementById("todo-input") as HTMLInputElement;
+const todoList = document.getElementById("todo-list");
+const todoAddBtn = document.getElementById("todo-add-btn");
+const todoInvalidFeedback = document.querySelector(".invalid-feedback");
+console.log(todoInvalidFeedback);
+todoList.addEventListener("change", (e) => {
+  const target = e.target as HTMLInputElement;
+  if (target.matches(`input.is-complete`)) {
+    console.log(
+      (target.closest(`li.to-do-item`) as HTMLElement).dataset.toDoId
+    );
+    handleIsCompleteChange(
+      Number((target.closest(`li.to-do-item`) as HTMLElement).dataset.toDoId)
+    );
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  const target = e.target as HTMLInputElement;
+  if (target.closest(`.todo-remove-btn`)) {
+    handleRemoveTodo(
+      Number((target.closest(`li.to-do-item`) as HTMLElement).dataset.toDoId)
+    );
+  }
+});
+
+function renderEmptyNotificationList() {
+  return ` <div class="bg-light rounded text-center p-3">
+  <h1 class="display-3 fw-bold">Well done!!!</h1>
+  <p>you are all done for today, go titfanen on the beach</p>
+</div>`;
+}
+
+function renderTodoItem({ id, title, isCorrect }: Todo) {
+  return `
+  <li data-to-do-id="${id}" class="to-do-item list-group-item d-flex justify-content-between">
+    <input
+      class="form-check-input me-1 is-complete"
+      type="checkbox"
+      id="todo-${id}"
+    
+      ${isCorrect ? "checked" : ""}
+    />
+   
+   
+    <label
+      class="form-check-label mx-1 flex-fill  ${
+        isCorrect ? `text-muted text-decoration-line-through` : ""
+      }"
+      for="todo-${id}"
+    >
+      ${title}
+    </label>
+    <button class=" todo-remove-btn bg-danger" >
+    <i class="bi bi-trash3"></i></button>
+    
+  </li>`;
+}
+function renderList(toDos: Todo[] = []) {
+  toDos.sort((a, b) => Number(a.isCorrect) - Number(b.isCorrect));
+  let html = `<ul class="list-group">`;
+  for (const todo of toDos) {
+    html += renderTodoItem(todo);
+  }
+  html += `</ul>`;
+  return html;
+}
+renderToApp();
+todoAddBtn.addEventListener("click", handleAddTodo);
+todoInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    handleAddTodo();
+  }
+});
+function handleRemoveTodo(id: number) {
+  removeToDos(id);
+  renderToApp();
+}
+function renderToApp() {
+  const toDos = getToDos();
+  todoList.innerHTML =
+    toDos.length > 0 ? renderList(toDos) : renderEmptyNotificationList();
+}
+function handleAddTodo() {
+  renderError();
+  try {
+    addToDos(todoInput.value);
+    todoInput.value = "";
+    renderToApp();
+  } catch (err) {
+    renderError(err.message);
+  }
+}
+function renderError(err?: string) {
+  if (!err) {
+    todoInput.classList.remove("is-invalid");
+    todoInvalidFeedback.innerHTML = "";
+    return;
+  }
+
+  todoInput.classList.add("is-invalid");
+  todoInvalidFeedback.innerHTML = err;
+}
+function handleIsCompleteChange(id: number) {
+  changeToDos(id);
+  renderToApp();
+}
